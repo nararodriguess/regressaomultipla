f8285bafed5fd8468dee142dfb34a478950ea697
Nara <79164423+nararodriguess@users.noreply.github.com>
2022-09-07 10:52:25 -0300

    Update regressao.R

--- regressao.R
+++ regressao.R
@@ -1,5 +1,3 @@
-#Regress„o Linear Multipla
-
 rm(list=ls()) #limpa a memoria do R
 
 
@@ -7,72 +5,89 @@ rm(list=ls()) #limpa a memoria do R
 
 if (! require (pacman)) install.packages("pacman")
 library(pacman)
-pacman:: p_load(GGally, lmtest, plotly)
+pacman:: p_load(GGally, lmtest, robustX, sandwich)
 
 
-#Plotando um gr·fico de dispers„o usando ggplot
-ggplot(data = dados, aes(y= lucro_por_acao, x=roa)) + geom_point()
+# Descrevendo os dados
+summary(dados)
 
-#Adicionando uma reta de regressao com o argumento geom_smooth
-gg  = ggplot(data = dados, aes(y= lucro_por_acao, x=roa)) + geom_point()
-gg + geom_smooth(method = 'lm')
-
-# Plota um gr·fico de dispers„o'dinamico'
-ggplotly(gg + geom_smooth(method = 'lm')) 
-
-
-#CorrelaÁ„o entre vari·veis
-cor(dados$lucro_por_acao, dados$ebitda)
-cor(dados$lucro_por_acao, dados$div_tot_liq)
-cor(dados$lucro_por_acao, dados$liq_geral)
-cor(dados$lucro_por_acao, dados$marg_liq)
-cor(dados$lucro_por_acao, dados$roa)
-cor(dados)
-
-
-#Gr·fico de calor da correlaÁ„o 
+#Gr√°fico de calor da correla√ß√£o 
 ggcorr(dados, label=T, label_round = 3)
 
 
-# Plotando gr·fico 3D entre lucro, roa e margem
-plot_ly(dados, z= ~lucro_por_acao,
-        x = ~roa,
-        y = ~marg_liq) %>% add_markers()
+#------------------------------------------------
 
+# Verificando e corrigindo outliers
+
+par(mfrow = c(1,3))
+boxplot(dados$liq_cor, outline = T)
+boxplot(dados$roa, outline = T)
+boxplot(dados$marg_liq, outline = T)
+
+# Corrigindo outliers
+install.packages('robustX')
+library(robustX)
+
+# Filtra os dados das colunas especificadas para retornar false para dados considerados outliers
+outliers = robustX::mvBACON(dados[c("roa", "marg_liq")], alpha = 0.95, init.sel = "Mahalanobis")
+
+# Adiciona uma coluna chamada outliers com true ( n√£o outlier) e false ( outlier)
+dados_out = within(dados, {outliers = outliers$subset})
+
+# Identificando quantidade de observa√ß√µes 
+table(dados_out$outliers)
+
+# Excluindo outliers
+dados_out = subset(dados_out, outliers == TRUE)
+dados_out$outliers = NULL
+remove(outliers)
+
+# Comparando dados
+par(mfrow = c(2,2))
+boxplot(dados$roa, outline = T)
+boxplot(dados$marg_liq, outline = T)
+boxplot(dados_out$roa)
+boxplot(dados_out$marg_liq, outline = T)
+
+
+#------------------------------------------------
 
 # Gerando histograma de cada variavel
-par(mfrow = c(2,3))
-hist(dados$lucro_por_acao)
-hist(dados$ebitda)
-hist(dados$div_tot_liq)
-hist(dados$liq_geral)
-hist(dados$marg_liq)
-hist(dados$roa)
+par(mfrow = c(1,3))
+hist(dados_out$roa)
+hist(dados_out$marg_liq)
+hist(dados_out$liq_cor)
 
+shapiro.test(dados_out$roa)
+shapiro.test(dados_out$marg_liq)
+shapiro.test(dados_out$liq_cor)
 
+#-------------------------------------------------
 
-# Ajustando o modelo de regress„o linear multipla
-modelo = lm(lucro_por_acao ~ ebitda + div_tot_liq +liq_geral + marg_liq + roa, data = dados)
+# Criando modelo
+modelo = lm(roa ~ marg_liq + liq_cor, data = dados_out)
 summary(modelo)
-
-
-
-# Criando um novo modelo
-modelo2 = lm(lucro_por_acao ~ marg_liq + roa, data = dados)
-summary(modelo2)
-
 
 # Testando pressupostos
 par(mfrow = c(2,2))
-plot(modelo2)
-
+plot(modelo)
 
 # Teste de normalidade de Shapiro
-shapiro.test(modelo2$residuals)
+shapiro.test(modelo$residuals)
 
-# Teste de outliers
-summary(rstandard(modelo2))
+# Testando outliers 
+summary(rstandard(modelo))
+par(mfrow = c(1,1))
+boxplot(modelo$residuals, outline = T)
 
+# Teste de multicolinearidade 
+vif(modelo)
 
 #Teste de homocedasticidade
-bptest(modelo2)
+bptest(modelo)
+
+#Criando modelo com controle para heterocedasticidade
+library(sandwich)
+
+dados_het = coeftest(modelo, vcov. = vcovHC, type = "HC1")
+dados_het
